f000913af257b78d312c136612291db97ad0138c
gguleid <gguleid@gmail.com>
2021-06-10 15:33:08 -0400

    setup for controller file

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+const bcryot = require('bcrypt');
+const express = require('express');
+const userRouter = express.Router();
+const User = require('../models/user');
+
+// New (registration page)
+
+// Create (registration route)
+
+// Export User Router
+module.exports = userRouter;
